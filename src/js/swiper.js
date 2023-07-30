0303f9cc0deab4d3c49a4d46956bfade313e24da
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-07-31 01:27:26 +0300

    1

---
+++ src/js/swiper.js
@@ -0,0 +1,60 @@
+import Swiper from 'swiper'; 
+import { Navigation, Pagination } from 'swiper/modules'; 
+// import 'swiper/css'; 
+// import 'swiper/css/navigation'; 
+// import 'swiper/css/pagination'; 
+ 
+const yachtsSlider = new Swiper('#yachts-slider', { 
+  // Optional parameters 
+  // direction: "horizontal", // "horizontal", "vertical" 
+  speed: 650, // швидкість перемикання слайду 
+  // effect: "fade", // slider, cards, coverflow, flip, fade, cube 
+  initialSlide: 0, // початок з 2-го слайду 
+  // freemode: true, // дозволяє перетаскувати мишкою 
+  spaceBetween: 16, 
+  loop: true, // true - круговий слайдер, false - зкінцевим положенням 
+ 
+  //centeredSlides: false, // центрування слайду 
+  modules: [Navigation], 
+  // Navigation arrows 
+  navigation: { 
+    nextEl: '.swiper-button-next', 
+    prevEl: '.swiper-button-prev', 
+  }, 
+  breakpoints: { 
+    375: { 
+      slidesPerView: 1, 
+    }, 
+    768: { 
+      slidesPerView: 2, 
+    }, 
+    1280: { 
+      slidesPerView: 3, 
+    }, 
+  }, 
+}); 
+ 
+const reviewsSlider = new Swiper('#reviews-slider', { 
+  clickable: true, 
+  watchOverflow: true, 
+ 
+  modules: [Pagination], 
+  pagination: { 
+    el: '.swiper-pagination', 
+  }, 
+ 
+  breakpoints: { 
+    375: { 
+      slidesPerView: 1, 
+      spaceBetween: 16, 
+    }, 
+    768: { 
+      slidesPerView: 2, 
+      spaceBetween: 16, 
+    }, 
+    1280: { 
+      slidesPerView: 3, 
+      spaceBetween: 32, 
+    }, 
+  }, 
+});
